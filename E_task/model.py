5089b110c858f871b3a0e77bb6ddc98479f773b1
Mattia Evangelisti <71966068+MadMatti@users.noreply.github.com>
2023-05-09 18:17:46 +0200

    Binary classification done 99 acc

--- E_task/model.py
+++ E_task/model.py
@@ -1,15 +1,24 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18
-from torchvision.models.resnet import ResNet18_Weights
+from torchvision.models import resnet18, resnet50, resnet34
+from torchvision.models.resnet import ResNet18_Weights, ResNet34_Weights, ResNet50_Weights
 
 
 class OxfordPetsModel(nn.Module):
     def __init__(self, num_classes: int):
         super().__init__()
+
         self.num_classes = num_classes
-        self.model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        # self.model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        # self.model = resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
+        self.model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+        for param in self.model.fc.parameters():
+            param.requires_grad = True
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
--- E_task/train.py
+++ E_task/train.py
@@ -9,19 +9,39 @@ DATASET_PATH = '/Users/mattiaevangelisti/Documents/OxfordPetsDataset'
 
 def load_dataset(dataset):
     dataset = OxfordPetsDataset(root=DATASET_PATH, transform=get_transforms())
-    train_length = int(len(dataset) * 0.8)
-    val_length = len(dataset) - train_length
-    train_dataset, val_dataset = random_split(dataset, [train_length, val_length])
+    train_size = int(0.75 * len(dataset))
+    val_test_size = len(dataset) - train_size
+    val_size = test_size = int(val_test_size / 2)
+    train_dataset, val_test_dataset = random_split(dataset, [train_size, val_test_size])
+    val_dataset, test_dataset = random_split(val_test_dataset, [val_size, test_size])
     
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-    return train_loader, val_loader
+    return train_loader, val_loader, test_loader
+
+def test_model(model, dataloader, device):
+    model.eval()
+    running_corrects = 0
+
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            running_corrects += torch.sum(preds == labels.data)
+
+    accuracy = 100 * running_corrects / len(dataloader.dataset)
+    print(f'Test Accuracy: {accuracy:.4f}')
 
 def train(model, num_epochs, dataset, device):
-    train_loader, val_loader = load_dataset(dataset)
+    train_loader, val_loader, test_loader = load_dataset(dataset)
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
 
     for epoch in tqdm(range(num_epochs)):
         model.train()
@@ -46,7 +66,10 @@ def train(model, num_epochs, dataset, device):
         accuracy = 100 * correct / total
         print(f'Epoch: {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}, Accuracy: {accuracy:.2f}%')
 
-    torch.save(model.state_dict(), 'resnet18_binary.pth')
+    test_model(model, test_loader, device)
+    # torch.save(model.state_dict(), 'resnet18_binary.pth')
+    # torch.save(model.state_dict(), 'resnet34_binary.pth')
+    torch.save(model.state_dict(), 'resnet50_binary.pth')
 
 
 if __name__ == '__main__':
@@ -54,6 +77,7 @@ if __name__ == '__main__':
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     '''For macOS'''
     device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
-    print(device)
     model = OxfordPetsModel(num_classes=2).to(device)
+
+    print("The model will be running on", device, "device\n")
     train(model, num_epochs=10, dataset=OxfordPetsDataset, device=device)
